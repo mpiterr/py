cf65d31fc514ef3cebf5ba02d1f91d308bb01e1d
mpiterr <73519537+mpiterr@users.noreply.github.com>
2021-01-24 23:23:52 +0100

    Add files via upload

--- zad5.py
+++ zad5.py
@@ -1,8 +1,9 @@
 from random import randint
-#1
+
+# 1
 imiona = ['Damian', 'Ola', 'Barbara', 'Robert', 'Zygmunt', 'Ewa']
-imiona = [imiona [0] for imiona in imiona]
+imiona = [imiona[0] for imiona in imiona]
 print(imiona)
-#2
+# 2
 los = [[randint(1, 10) for i in range(4)] for j in range(5)]
 print(los)
